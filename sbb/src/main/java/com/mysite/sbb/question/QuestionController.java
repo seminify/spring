70180e80e8a27544083b7b92e0e66c13c4b8f6be
seminify <seminify@gmail.com>
2022-06-02 14:04:35 +0900

    질문 상세 컨트롤러 만들기

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -18,5 +19,10 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping(value = "/question/detail/{id}")
+    public String detail(Model model,@PathVariable("id") Integer id){
+        return "question_detail";
     }
 }
