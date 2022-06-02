ccf33871b48cab3d74ab3d7a2c423fca8343cb18
seminify <seminify@gmail.com>
2022-06-02 14:44:00 +0900

    답변 저장하기

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -17,10 +17,12 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class AnswerController {
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id  ") Integer id, @RequestParam String content) {
         Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, content);
         return String.format("redirect:/question/detail$s", id);
     }
 }
