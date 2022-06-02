241127f85de93a8839dd2f4b135e7b650e548b29
seminify <seminify@gmail.com>
2022-06-02 13:35:55 +0900

    답변에 연결된 질문 찾기 vs 질문에 달린 답변 찾기

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,8 +3,10 @@ package com.mysite.sbb;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +17,14 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
-	@Autowired
-	private AnswerRepository answerRepository;
-
+	@Transactional
 	@Test
 	void testJpa() {
-		Optional<Answer> oa = this.answerRepository.findById(1);
-		assertTrue(oa.isPresent());
-		Answer a = oa.get();
-		assertEquals(2, a.getQuestion().getId());
+		Optional<Question> oq=this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q=oq.get();
+		List<Answer> answerList=q.getAnswerList();
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 }
