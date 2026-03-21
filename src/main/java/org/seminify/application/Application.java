3532c7fafb1b4ce6954a69c867d0837a8c982822
seminify <seminify@gmail.com>
2026-03-21 13:45:49 +0000

    2026-03-21

---
+++ src/main/java/org/seminify/application/Application.java
@@ -0,0 +1,12 @@
+package org.seminify.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.FullyQualifiedConfigurationBeanNameGenerator;
+
+@SpringBootApplication(nameGenerator = FullyQualifiedConfigurationBeanNameGenerator.class)
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
