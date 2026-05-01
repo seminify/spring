a3b563cbc21050827cd23073444eac5a0e1d5fbe
seminify <seminify@gmail.com>
2026-05-01 17:48:17 +0900

    2026-05-01

---
+++ src/test/kotlin/org/seminify/application/ApplicationTests.kt
@@ -0,0 +1,11 @@
+package org.seminify.application
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
